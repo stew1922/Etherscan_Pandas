deded259bd0cc9a6b48ea5ad6debcc80ab7cb211
stew1922 <stew1922@gmail.com>
2022-04-14 12:38:15 -0500

    added a financial warning to the ReadMe.md - more readme info to come later

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Etherscan_Analytics
 A simple code base to utilize Etherscan's API endpoints to perform analytics on Ether or any tokens on the Ethereum network
+
+- No warranty is made to the accuracy of this code or the results it produces.  Any investment outcomes that utilize this data are solely the responsibility of the investor. 
--- chedda.py
+++ chedda.py
@@ -1,29 +1,18 @@
 import pandas as pd
-from dotenv import load_dotenv
-load_dotenv
-import os
 import etherscan
 
-
-#be sure a .env file is saved with a parameter "API_KEY" with the appropriate key for your account with Etherscan.io
-#pull in the API_KEY
-api_key = os.getenv('API_KEY')
-
-#create the connection to the Etherscan API
-# eth = Etherscan(api_key)
-
-##identify some key CHEDDA wallets/contracts:
-#The CHEDDA "dead" wallet -- initial 50B token burn and then the addtional 1B token burn were here
+## identify some key CHEDDA wallets/contracts:
+# The CHEDDA "dead" wallet -- initial 50B token burn and then the addtional 1B token burn were here
 chedda_dead = "0x000000000000000000000000000000000000dead"
-#The CHEDDA deployer wallet
+# The CHEDDA deployer wallet
 chedda_deployer = "0x763cfa5b0eb7bb2e54100010c5a8fc44d1dde714"
-#The dev wallet (staking rewards are coming from here)
+# The dev wallet (staking rewards are coming from here)
 dev_wallet = "0x8d29e90625213f4eedbdc49e6358fdbf76b9ddd0"
-#The contract address for CHEDDA
+# The contract address for CHEDDA
 chedda_contract = "0x16756EC1DEb89A2106C35E0B586a799D0A61837D"
-#The marketing wallet where 2% of all transactions go to cover marketing expenses
+# The marketing wallet where 2% of all transactions go to cover marketing expenses
 chedda_marketing = "0x9625088c654d26b9132feb52d37107ab898d19c6"
-#The ETH null address - 1% of all transactions are sent here to be burned
+# The ETH null address - 1% of all transactions are sent here to be burned
 null_address = "0x0000000000000000000000000000000000000000"
 
 # instantiate the classes
@@ -38,9 +27,9 @@ total_supply = Stats.erc20_token_supply(chedda_contract)
 #the number of tokens burned by the devs
 dev_burned_chedda = Accounts.erc20_token_balance(chedda_contract, chedda_dead)
 #the number of tokens burned by transactions
-trans_burned_chedda = initial_supply - total_supply # for now until Etherscan fixes the balance issue on the null address
+txn_burned_chedda = initial_supply - total_supply # for now until Etherscan fixes the balance issue on the null address
 #circulating supply are the total number of token on the market and in CHEDDA wallets
-circulating_supply = initial_supply - dev_burned_chedda - trans_burned_chedda
+circulating_supply = initial_supply - dev_burned_chedda - txn_burned_chedda
 
 #create a function to automatically update CHEDDA_history.csv
 def update_chedda_history(file="./CHEDDA_history.csv", rate_limit=5):
